e8906939c9dcc40ce19ce0146b0f503510a98467
6e0d0a <gaizka.gg@outlook.es>
2023-01-17 12:14:43 +0100

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM php:8.0-apache
 
-RUN docker-php-ext-install pdo pdo_pgsql
+RUN apt-get install --no-install-recommends -y libpq-dev && \
+    docker-php-ext-install pdo pgsql pdo_pgsql
 
 EXPOSE 80/tcp
 
--- public/index.php
+++ public/index.php
@@ -3,9 +3,11 @@
 $db_host = getenv('dbhost');
 $db_port = getenv('dbport');
 $db_name = getenv('dbname');
+$db_user = getenv('dbuser');
 $db_pass = getenv('dbpass');
 
 var_dump($db_host);
 var_dump($db_port);
 var_dump($db_name);
+var_dump($db_user);
 var_dump($db_pass);
